793424d206446e875b2072036f3f5ffd25a9c00b
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-08-02 16:19:18 +0300

    add connection to db

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import settings
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = settings.db_url
+db = SQLAlchemy(app)
 
 with app.app_context():
     from . import routes
