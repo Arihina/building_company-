19fb1312454e5df2248a39abac6776f52fba09f7
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-08-03 18:33:23 +0300

    add client and enployee classes

--- app/models.py
+++ app/models.py
@@ -11,6 +11,25 @@ class Base(db.Model):
         return f'<{self.__class__.__name__} {', '.join(cols)}>'
 
 
+class Employee(Base):
+    __tablename__ = 'employee'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    post: Mapped[str]
+    phone_number: Mapped[str]
+    full_name: Mapped[str]
+    email: Mapped[str]
+
+
+class Client(Base):
+    __tablename__ = 'client'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    phone_number: Mapped[str]
+    full_name: Mapped[str]
+    organization_name: Mapped[str]
+
+
 class Product(Base):
     __tablename__ = 'product'
 
