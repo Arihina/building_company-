31c01db53789d23b0b9db0c0a379ad8df65da66b
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-08-05 21:06:58 +0300

    upd

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 from config import settings
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = settings.db_url
 app.config['SECRET_KEY'] = settings.app_secret_key
 
