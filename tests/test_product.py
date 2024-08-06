41fbb6548e17a095513db09a3abda51b904578f8
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-08-06 22:49:25 +0300

    add tests for product model

--- tests/test_product.py
+++ tests/test_product.py
@@ -4,24 +4,25 @@ import unittest
 import requests
 
 
-class ClientTestCase(unittest.TestCase):
-    BASE_URL = 'http://127.0.0.1:5000/clients'
+class ProductTestCase(unittest.TestCase):
+    BASE_URL = 'http://127.0.0.1:5000/products'
 
-    def test_get_client(self):
-        client_id = 26
-        url = f'{self.BASE_URL}/{client_id}'
+    def test_get_product(self):
+        product_id = 2
+        url = f'{self.BASE_URL}/{product_id}'
 
         response = requests.get(url)
         self.assertEqual(response.status_code, 200)
 
         response_data = response.json()
-        self.assertIn('client', response_data)
-        client = response_data['client']
-        self.assertIn('full_name', client)
-        self.assertIn('phone_number', client)
-        self.assertIn('organization_name', client)
+        self.assertIn('product', response_data)
+        product = response_data['product']
+        self.assertIn('name', product)
+        self.assertIn('type', product)
+        self.assertIn('unit_type', product)
+        self.assertIn('price', product)
 
-    def test_get_clients(self):
+    def test_get_products(self):
         url = self.BASE_URL
 
         response = requests.get(url)
@@ -30,13 +31,14 @@ class ClientTestCase(unittest.TestCase):
         response_data = response.json()
         self.assertIsInstance(response_data, list)
 
-    def test_post_clients(self):
+    def test_post_product(self):
         url = self.BASE_URL
         headers = {'Content-Type': 'application/json'}
         data = {
-            "full_name": "New test client",
-            "phone_number": "0987654321",
-            "organization_name": "test org 2"
+            'name': "New test product",
+            'type': "type",
+            'unit_type': "unit",
+            'price': 12.7
         }
 
         response = requests.post(url, headers=headers, data=json.dumps(data))
@@ -45,12 +47,12 @@ class ClientTestCase(unittest.TestCase):
         response_data = response.json()
         self.assertEqual(response_data['message'], 'CREATED')
 
-    def test_put_client(self):
-        client_id = 26
-        url = f'{self.BASE_URL}/{client_id}'
+    def test_put_product(self):
+        product_id = 22
+        url = f'{self.BASE_URL}/{product_id}'
         headers = {'Content-Type': 'application/json'}
         data = {
-            "full_name": "No test client"
+            'price': 127.3
         }
 
         response = requests.put(url, headers=headers, data=json.dumps(data))
@@ -59,19 +61,19 @@ class ClientTestCase(unittest.TestCase):
         response_data = response.json()
         self.assertEqual(response_data['message'], 'UPDATED')
 
-    def test_no_found(self):
-        client_id = 500
-        url = f'{self.BASE_URL}/{client_id}'
-
-        response = requests.get(url)
-        self.assertEqual(response.status_code, 404)
-
-    def test_del_client(self):
-        client_id = 26
-        url = f'{self.BASE_URL}/{client_id}'
+    def test_del_product(self):
+        product_id = 22
+        url = f'{self.BASE_URL}/{product_id}'
 
         response = requests.delete(url)
         self.assertEqual(response.status_code, 204)
+
+    def test_no_found(self):
+        product_id = 500
+        url = f'{self.BASE_URL}/{product_id}'
+
+        response = requests.get(url)
+        self.assertEqual(response.status_code, 404)
 
 
 if __name__ == '__main__':
