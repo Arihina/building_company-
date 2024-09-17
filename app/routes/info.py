ec150c3acc750d60f03584687430f8cbd83ef27a
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-09-17 14:26:53 +0300

    upd

--- app/routes/info.py
+++ app/routes/info.py
@@ -1,19 +1,9 @@
-from flask import Blueprint, jsonify
-from sqlalchemy import text
-
-from .. import db
+from flask import Blueprint, render_template
 
 info_bp = Blueprint('info_bp', __name__)
 
 
+@info_bp.route('/about')
 @info_bp.route('/')
 def index():
-    version_query = db.session.execute(text("SELECT version()")).fetchone()
-    postgres_version = version_query[0] if version_query else "Unknown"
-
-    data = {
-        'postgresql version': postgres_version,
-        'flask version': '3.0.3'
-    }
-
-    return jsonify(data)
+    return render_template('about.html'), 200
