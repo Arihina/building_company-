e3c50e47775e5dd55b49f2121215cdc7bbcaa789
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-09-17 14:19:05 +0300

    add admin page

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 from .. import logger
 
@@ -8,4 +8,4 @@ admin_bp = Blueprint('admin_bp', __name__)
 @admin_bp.route('/admin', methods=['GET'])
 def admin_page():
     logger.debug('/admin')
-    return 'admin page'
+    return render_template('admin_page.html'), 200
